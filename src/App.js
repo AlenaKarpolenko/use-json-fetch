649b30f3e672946cbece31a9069af92e1f3ababd
AlenaKarpolenko <alenac190590@mail.ru>
2022-12-02 17:01:50 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 import './App.css';
-import VideoList from './components/VideoList';
+import List from './components/List';
+
+const list = [
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+    views: 50
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+    views: 12
+  },
+  {
+    type: 'article',
+    title: 'Невероятные события в неизвестном поселке...',
+    views: 175
+  },
+  {
+    type: 'article',
+    title: 'Секретные данные были раскрыты!',
+    views: 1532
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+    views: 4253
+  },
+  {
+    type: 'article',
+    title: 'Кот Бегемот обладает невероятной...',
+    views: 12,
+  },
+];
 
 function App() {
-    const list = [
-        {
-            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2021-08-15 19:11:00'
-        },
-        {
-            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2021-08-15 14:07:00'
-        },
-        {
-            url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2018-02-03 23:16:00'
-        },
-        {
-            url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2018-01-03 12:10:00'
-        },
-        {
-            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2018-01-01 16:17:00'
-        },
-        {
-            url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2017-12-02 05:24:00'
-        },
-    ];
-
-    return (
-        <VideoList list={list} />
-    );
+  return (
+    <List list={list} />
+  );
 }
 
 export default App;
--- src/components/VideoList.js
+++
@@ -1,5 +0,0 @@
-import Video from "./Video";
-
-export default function VideoList(props) {
-  return props.list.map(item => <Video key={Math.random(1000000)} url={item.url} date={item.date} />);
-}
