e9abfa8bfe24080c5a817dc295b07b9055972b4b
AlenaKarpolenko <alenac190590@mail.ru>
2022-12-06 16:53:42 +0700

    4

--- src/App.js
+++ src/App.js
@@ -1,79 +1,118 @@
 import React, { useState } from "react";
 import "./App.css";
-import moment from "moment";
-import "moment/locale/ru";
 
-function DateTime(props) {
-  return <p className="date">{props.date}</p>;
+function New(props) {
+  return (
+    <div className="wrap-item wrap-item-new">
+      <span className="label">New!</span>
+      {props.children}
+    </div>
+  );
 }
 
-const Data = (Component) => {
-  function Date(props) {
-    let now = moment();
-    let end = moment(props.date);
-    let minutes = now.diff(end, "minutes");
-    let hours = now.diff(end, "hours");
-    let days = now.diff(end, "days");
-    let new_date = "";
-    if (minutes < 60) {
-      new_date = `${minutes} минут назад`;
-    } else if (hours < 24) {
-      new_date = `${hours} часов назад`;
+function Popular(props) {
+  return (
+    <div className="wrap-item wrap-item-popular">
+      <span className="label">Popular!</span>
+      {props.children}
+    </div>
+  );
+}
+
+const HighlightNew = (Component) => {
+  function View(props) {
+    if (props.views >= 1000) {
+      return (
+        <Popular>
+          <Component {...props} />
+        </Popular>
+      );
+    } else if (props.views < 100) {
+      return (
+        <New>
+          <Component {...props} />
+        </New>
+      );
     } else {
-      new_date = `${days} дней назад`;
+      return <Component {...props} />;
     }
-    return <Component date={new_date} />;
   }
-  return Date;
+  return View;
 };
 
-const DateTimePretty = Data(DateTime);
+function Article(props) {
+  return (
+    <div className="item item-article">
+      <h3>
+        <a href="#">{props.title}</a>
+      </h3>
+      <p className="views">Прочтений: {props.views}</p>
+    </div>
+  );
+}
 
 function Video(props) {
   return (
-    <div className="video">
+    <div className="item item-video">
       <iframe
         src={props.url}
         frameborder="0"
         allow="autoplay; encrypted-media"
         allowfullscreen
       ></iframe>
-      <DateTimePretty date={props.date} />
+      <p className="views">Просмотров: {props.views}</p>
     </div>
   );
 }
 
-function VideoList(props) {
-  return props.list.map((item) => <Video url={item.url} date={item.date} />);
+const HighNewVideo = HighlightNew(Video);
+const HighNewArticle = HighlightNew(Article);
+
+function List(props) {
+  return props.list.map((item) => {
+    switch (item.type) {
+      case "video":
+        return <HighNewVideo {...item} />;
+
+      case "article":
+        return <HighNewArticle {...item} />;
+    }
+  });
 }
 
 export default function App() {
   const [list, setList] = useState([
     {
+      type: "video",
       url: "https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2017-07-31 13:24:00",
+      views: 50,
     },
     {
+      type: "video",
       url: "https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2018-03-03 12:10:00",
+      views: 12,
     },
     {
-      url: "https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2018-02-03 23:16:00",
+      type: "article",
+      title: "Невероятные события в неизвестном поселке...",
+      views: 175,
     },
     {
-      url: "https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2018-01-03 12:10:00",
+      type: "article",
+      title: "Секретные данные были раскрыты!",
+      views: 1532,
     },
     {
+      type: "video",
       url: "https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2018-01-01 16:17:00",
+      views: 4253,
     },
     {
-      url: "https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2017-12-02 05:24:00",
+      type: "article",
+      title: "Кот Бегемот обладает невероятной...",
+      views: 12,
     },
   ]);
 
-  return <VideoList list={list} />;
+  return <List list={list} />;
 }
